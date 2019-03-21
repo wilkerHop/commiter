59611f9b8b2aa1ffe1f5dc1112fbd219bba9f036
Wilker Ribeiro <rekliw97@gmail.com>
2019-03-21 05:39:10 -0300

    first commit

---
+++ commiter.ps1
@@ -0,0 +1,28 @@
+param(
+    [string]$message
+)
+
+## Aplication to commit autoamtically with *consistent* messages 
+git add .
+
+if (!($message -eq $null)){
+    return git commit -m "$message"
+}
+
+$OutputVariable = (git status) | Out-String
+
+if ($OutputVariable.Contains('nothing to commit, working tree clean')) {
+    return Write-Output "Everything's fine!"
+}
+
+$files = $OutputVariable.Split([Environment]::NewLine) |
+    # maybe do a split by /                          .Split("\\|\/")[-3] -join ''
+    ForEach-Object { If (!($_ -eq $null)) { $_.Trim() } }
+
+$files = $files -join ' ' -split '  '
+
+$message = ($files[6..($files.length - 1)] -join ' ').Trim().Replace('  ', ' ') 
+
+$message | Write-Output
+
+git commit -m "$message"
